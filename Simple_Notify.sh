471f6ca729df43461976654ccdfadc94c9216e1a
NNormous <saniapply@gmail.com>
2016-07-07 14:07:48 +0800

    added test script

---
+++ Simple_Notify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while [ true ]
+do
+    notify-send "======================================== 30 Minutes Notification ========================================" "<b>TIME NOW IS `date +"%R"`</b>"
+    sleep 3600 
+done
